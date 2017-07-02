0c2e499d5dc9dba4127337554544320c4b2dc184
Evan <daley.evan@gmail.com>
2017-07-01 21:29:57 -0700

    More modeling

---
+++ Scifi/Assets/Cinematic/ModelData/Scripts/TimeControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour {
+
+	public float timeScale = 1f;
+
+	void Start () {
+		Time.timeScale = timeScale;
+	}
+	
+	void Update () {
+		
+	}
+}
